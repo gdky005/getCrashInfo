5c1ad78b86e2f0c09c438af84a613d08eccafab5
wangqing <wangqing@kaolafm.com>
2015-12-05 19:58:54 +0800

    1.使用gradle 构建 groovy,自动打包生成可执行的jar文件;

---
+++ src/main/groovy/gdky005/run/jarfile/TestJar.groovy
@@ -0,0 +1,35 @@
+package gdky005.run.jarfile
+
+import com.alee.laf.WebLookAndFeel
+import com.alee.laf.button.WebButton
+import com.alee.laf.rootpane.WebFrame
+
+import javax.swing.*
+import java.awt.*
+/**
+ * Created by WangQing on 15/12/5.
+ */
+
+WebFrame webFrame = initFrame()
+
+webFrame.add(new WebButton("hello"))
+
+show(webFrame)
+
+private show(WebFrame webFrame) {
+    webFrame.setVisible(true)
+}
+
+
+private WebFrame initFrame() {
+
+    WebLookAndFeel.install()
+
+    WebFrame frame = new WebFrame("Title")
+    frame.setTopBg(Color.blue)
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 设置关闭的事件
+    frame.setLocation(500, 150)
+    frame.setSize(300, 300)
+
+    frame
+}
